636bf1ca83966f3e2875d3b7a69063a9719163fe
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-05-29 14:33:57 -0500

    I work in PHP too much

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,7 @@ def deploy(giturl, gitrepo, gitbranch, deploydir):
   print "===> Checking that deployment directory exists"
   if not exists(deploydir, use_sudo=False, verbose=True):
     print "===> Creating deployment directory"
-    run("mkdir -p %s" $(deploydir))
+    run("mkdir -p %s" %(deploydir))
   print "===> Pulling updated code from git repository"
   with prefix("cd %s" %(deploydir)):
     run("git pull --rebase --verbose")
