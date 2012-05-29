6500fedcf077d2519f5cc83234c6efe5e0b2d468
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-05-29 14:23:01 -0500

    Added deployment directory creation

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import *
+from fabric.contrib.files import exists
 import time
 
 env.user = 'bender'
@@ -6,6 +7,10 @@ env.shell = '/bin/bash -c'
 
 # Checkout updated code
 def deploy(giturl, gitrepo, gitbranch, deploydir):
+  print "===> Checking that deployment directory exists"
+  if not exists(deploydir, use_sudo=False, verbose=True):
+    print "===> Creating deployment directory"
+    run("mkdir -p %s" $(deploydir))
   print "===> Pulling updated code from git repository"
   with prefix("cd %s" %(deploydir)):
     run("git pull --rebase --verbose")
